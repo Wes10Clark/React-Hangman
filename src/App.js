0453e7e2dc0442821c7f159e46180359b25a0059
Weston <Wes10clark@gmail.com>
2023-02-07 19:52:13 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./Components/Header";
+import Figure from "./Components/Figure";
+import WrongLetters from "./Components/WrongLetters";
+import Word from "./Components/Word";
+
+const words = ["application", "programming", "interface", "wizard"];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+let payable = true;
 
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+    </>
   );
 }
 
---
+++ src/Components/WrongLetters.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WrongLetters = () => {
+  return (
+    <div className="wrong-letters-container">
+      <div></div>
+    </div>
+  );
+};
+
+export default WrongLetters;
